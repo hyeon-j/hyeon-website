e894d7d5e599d4c39850ebfa742d6a0fc4667113
hyeon-j <hyeonjoonpw@gmail.com>
2022-11-04 02:37:25 -0400

    Worked on API Page. Did not push in a while. A lot of changes.

--- src/pages/Api/Api.js
+++ src/pages/Api/Api.js
@@ -1,40 +1,176 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
+
+import ISO6391 from "iso-639-1";
 
 import "./Api.css";
 
-import { fetchCoronaData, apiList } from "./apiFunctions";
+import { coronaOptions, getLanguageOptions } from "./apiFunctions";
 
 export default function Api() {
+    const languageForm = useRef();
+
     const [coronaData, setCoronaData] = useState([]);
     const [isLoading, setLoading] = useState(true);
 
+    const [translateLanguages, setTranslateLanguages] = useState([]);
+
     useEffect(() => {
-        (async () => {
-            const coronaResponse = await fetchCoronaData();
-            setCoronaData(coronaResponse);
+        coronaDataFetch();
+        translateLanguageFetch();
 
-            setLoading(false);
-        })();
-    }, [coronaData]);
+        setLoading(false);
+    }, [isLoading]);
 
-    const fetchCoronaDataAndUpdate = () => {
-        fetchCoronaData().then((data) => {
-            setCoronaData(data);
-        });
+    const coronaDataFetch = () => {
+        axios
+            .request(coronaOptions)
+            .then(function (response) {
+                const arr = [];
+                for (var data in response["data"]["data"])
+                    arr.push(response["data"]["data"][data]);
+
+                setCoronaData(arr);
+            })
+            .catch(function (error) {
+                console.error(error);
+            });
+    };
+
+    const translateLanguageFetch = () => {
+        axios
+            .request(getLanguageOptions)
+            .then(function (response) {
+                const languages = [];
+                for (let i = 0; i < response.data.data.languages.length; i++) {
+                    const languageCode =
+                        response.data.data.languages[i]["language"];
+                    if (ISO6391.getName(languageCode) != "")
+                        languages.push(languageCode);
+                }
+
+                setTranslateLanguages(languages);
+            })
+            .catch(function (error) {
+                console.error(error);
+            });
+    };
+
+    const translateText = (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const formProps = Object.fromEntries(formData);
+        console.log(formProps["translate__input"]);
+
+        const encodedParams = new URLSearchParams();
+        encodedParams.append("q", formProps["translate__input"]);
+        encodedParams.append("target", formProps["translate__lang"]);
+        encodedParams.append("source", "en");
+
+        const options = {
+            method: "POST",
+            url: "https://google-translate1.p.rapidapi.com/language/translate/v2",
+            headers: {
+                "content-type": "application/x-www-form-urlencoded",
+                "X-RapidAPI-Key":
+                    "8c9f96ab58mshad751c6174d50fap11163djsn6751e466b550",
+                "X-RapidAPI-Host": "google-translate1.p.rapidapi.com",
+            },
+            data: encodedParams,
+        };
+
+        axios
+            .request(options)
+            .then(function (response) {
+                console.log(response.data);
+            })
+            .catch(function (error) {
+                console.error(error);
+            });
     };
 
     if (isLoading) {
-        return <h1>loading</h1>;
+        return <h1>loading...</h1>;
     } else {
         return (
             <>
                 <div className="api__container">
                     <div className="api__contents">
                         <div className="api__app__container">
-                            <h1 className="api__app__title">
+                            <a
+                                href="https://rapidapi.com/googlecloud/api/google-translate1"
+                                className="api__app__title"
+                            >
+                                Language Traslation
+                            </a>
+                            <div className="api__translate__contents">
+                                <span className="api__app__description">
+                                    Placeholder for language translate
+                                </span>
+                                <div>
+                                    <form
+                                        ref={languageForm}
+                                        onSubmit={translateText}
+                                        className="translate__form"
+                                    >
+                                        <div className="translate__textsection">
+                                            <textarea
+                                                type="text"
+                                                rows="4"
+                                                name="translate__input"
+                                                placeholder="Enter text - Language Auto Detected"
+                                                className="translate__input"
+                                            />
+                                            <div>
+                                                <select
+                                                    name="translate__lang"
+                                                    id="translate__lang"
+                                                    className="translate__select"
+                                                >
+                                                    {translateLanguages.map(
+                                                        (lang, index) => {
+                                                            return (
+                                                                <option
+                                                                    value={lang}
+                                                                    key={index}
+                                                                >
+                                                                    {ISO6391.getName(
+                                                                        lang
+                                                                    )}
+                                                                </option>
+                                                            );
+                                                        }
+                                                    )}
+                                                </select>
+                                                <div className="contact__section">
+                                                    <textarea
+                                                        type="text"
+                                                        rows="4"
+                                                        name="temp"
+                                                        placeholder="Enter text"
+                                                        className="translate__input"
+                                                    />
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <input
+                                            className="translate__submit"
+                                            type="submit"
+                                            value="TRANSLATE"
+                                        />
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="api__app__container">
+                            <a
+                                href="https://rapidapi.com/KishCom/api/covid-19-coronavirus-statistics"
+                                className="api__app__title"
+                            >
                                 US Corona Virus Statistics
-                            </h1>
-                            <div className="api__app__contents">
+                            </a>
+                            <div className="api__corona__contents">
                                 <span className="api__app__description">
                                     Real time update on the progress of COVID-19
                                     virus in the United States. Statistics
@@ -54,13 +190,10 @@ export default function Api() {
                                         {coronaData[2] - coronaData[1]}
                                     </div>
                                     <div className="api__app__corona__data">
-                                        Statistic Reported Time: {coronaData[4]}
+                                        Reported Time: {coronaData[4]}
                                     </div>
                                 </div>
                             </div>
-                            <button onClick={fetchCoronaDataAndUpdate()}>
-                                Refresh
-                            </button>
                         </div>
                     </div>
                 </div>
--- src/pages/Api/apiFunctions.js
+++ src/pages/Api/apiFunctions.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // API functions
 
-const options = {
+export const coronaOptions = {
     method: "GET",
     url: "https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/total",
     params: { country: "USA" },
@@ -12,25 +12,28 @@ const options = {
     },
 };
 
-export const fetchCoronaData = () => {
-    return axios
-        .request(options)
-        .then(function (response) {
-            console.log(response);
-
-            const arr = [];
-            for (var data in response["data"]["data"])
-                arr.push(response["data"]["data"][data]);
-
-            return arr;
-        })
-        .catch(function (error) {
-            console.error(error);
-        });
+export const getLanguageOptions = {
+    method: "GET",
+    url: "https://google-translate1.p.rapidapi.com/language/translate/v2/languages",
+    headers: {
+        "X-RapidAPI-Key": "8c9f96ab58mshad751c6174d50fap11163djsn6751e466b550",
+        "X-RapidAPI-Host": "google-translate1.p.rapidapi.com",
+    },
 };
 
-// API HTML Rendering Descriptions
+// export const translateOptions = {
+//     method: "POST",
+//     url: "https://google-translate1.p.rapidapi.com/language/translate/v2",
+//     headers: {
+//         "content-type": "application/x-www-form-urlencoded",
+//         "Accept-Encoding": "application/gzip",
+//         "X-RapidAPI-Key": "8c9f96ab58mshad751c6174d50fap11163djsn6751e466b550",
+//         "X-RapidAPI-Host": "google-translate1.p.rapidapi.com",
+//     },
+//     data: encodedParams,
+// };
 
+// API HTML Rendering Descriptions
 export const apiList = [
     {
         title: "US Corona Virus Statistics",
